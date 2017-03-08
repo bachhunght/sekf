f4b7c2583d7631b46a8730545bbde845d565b014
hunght <bachhunght@gmail.com>
2017-03-08 14:31:50 +0700

    update theme

--- build/wp-content/themes/sekf/functions.php
+++ build/wp-content/themes/sekf/functions.php
@@ -214,3 +214,19 @@ function custom_messages($messages) {
 }
 
 add_filter('wpcf7_messages', 'custom_messages');
+
+
+/*
+   Debug preview with custom fields
+*/
+
+add_filter('_wp_post_revision_fields', 'add_field_debug_preview');
+function add_field_debug_preview($fields){
+   $fields["debug_preview"] = "debug_preview";
+   return $fields;
+}
+
+add_action( 'edit_form_after_title', 'add_input_debug_preview' );
+function add_input_debug_preview() {
+   echo '<input type="hidden" name="debug_preview" value="debug_preview">';
+}
